69e0286209b0070376c41a739c23b5452e204e29
Joe Gesualdo <joegesualdo@gmail.com>
2022-02-04 17:32:38 -0500

    Mess with example

--- examples/example.rs
+++ examples/example.rs
@@ -2,8 +2,8 @@ use stylize::{self, Modifier, StylizedString, Modifier::*, Color::*};
 
 use stylize::Stylizer;
 
-fn main() {
-    let first_name: String = String::from("joe");
+fn stylize_name(first: String, last: String) -> String {
+    let first_name: String = String::from(first);
     let first_name_stylized = StylizedString {
         text: first_name,
         modifiers: vec![
@@ -14,7 +14,7 @@ fn main() {
         background_color: RGB(100,55,200),
     };
 
-    let last_name: String = String::from("Gesualdo");
+    let last_name: String = String::from(last);
     let last_name_stylized = StylizedString {
         text: last_name,
         modifiers: vec![
@@ -24,11 +24,20 @@ fn main() {
         color: Green,
         background_color: RGB(5,55,100),
     };
-
-    println!(
+    format!(
         "{} {}",
         first_name_stylized.to_string(),
         last_name_stylized.to_string(),
+    )
+}
+
+fn main() {
+    println!(
+        "{}",
+        stylize_name(
+            String::from("peter"),
+            String::from("parker"),
+        )
     );
 }
 
